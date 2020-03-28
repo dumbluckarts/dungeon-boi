0a9e54a3a17fb0ce4052e65fe94a94232e5b909a
Nelson M <nelsontron@outlook.com>
2020-03-28 11:27:06 -0600

    Update inv.js

--- Files/inv.js
+++ Files/inv.js
@@ -1,10 +1,11 @@
 var inventory = (function() {
-	var data = {}
-	var self = {}
+	var data = {} // the inventory object
+	var self = {} // self object basically means "public." Thing visible by "inventory."
 
-	self.getItem = (item) => {
-		var result;
+	self.getItem = function(item) {
+		var result; // amount of inventory item to be returned
 
+		// loop through inventory item names
 		for (var i of Object.keys(data)) {
 			if (i == item) {
 				result = data[i];
@@ -14,11 +15,13 @@ var inventory = (function() {
 		return result;
 	}
 
-	self.exists = (item) => {
+	// returns boolean true or false if item exists
+	self.exists = function(item) {
 		return self.getItem(item) != undefined;
 	}
 
-	self.addItem = (item, amount) => {
+	// add item by amount
+	self.addItem = function(item, amount) {
 		if (self.exists(item)) {
 			data[item] += parseInt(amount)
 		}
@@ -27,7 +30,8 @@ var inventory = (function() {
 		}
 	}
 
-	self.addItems = (str) => {
+	// add items from a string like fireball=5,nelson=2
+	self.addItems = function(str) {
 		var arr = str.split(",")
 
 		for (var i of arr) {
@@ -38,7 +42,8 @@ var inventory = (function() {
 		}
 	}
 
-	self.removeItem = (item, amount) => {
+	// remove item by amount
+	self.removeItem = function(item, amount) {
 		if (self.exists(item)) {
 			data[item] -= amount
 
@@ -50,14 +55,18 @@ var inventory = (function() {
 		}
 	}
 
-	self.deleteItem = (item) => {
+	// delete an item
+	self.deleteItem = function(item) {
 		delete data[item]
 	}
 
-	self.print = () => {
+	// print out inventory data in console for testing purposes.
+	self.print = function() {
 		console.log(data)
 	}
 
+	// this function returns the variable "self" which contains all the functions I just added to
+	// said object. IE. self.print(), self.deleteItem(), self.addItem()
 	return self;
 })();
-var inv = inventory
+var inv = inventory // made an alias because im lazy
