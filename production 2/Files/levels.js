4ab455d8eb196782849b5074dd217c357a406f6d
Nelson M <nelsontron@outlook.com>
2020-08-20 21:23:47 -0600

    update enemies, added 3 more layouts

--- production 2/Files/levels.js
+++ production 2/Files/levels.js
@@ -17,6 +17,7 @@ function Picker(list) {
     }
 
     function getLast() {
+        console.log(last)
         return last ? last : "NONE"
     }
 
@@ -36,10 +37,13 @@ function Picker(list) {
 }
 
 var Leveler = Picker([
-    // 'Level 1',
+    'Level 1',
     'Level 2',
     'Level 3',
     'Level 4',
     'Level 5',
     'Level 6',
+    'Level 7',
+    'Level 8',
+    'Level 9',
 ])
